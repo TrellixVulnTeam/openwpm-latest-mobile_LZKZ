c8dec57bede50d75ce7e81ba08e8195bb16486b5
MaazBinMusa <maazbinmusa94@gmail.com>
2021-06-22 15:36:20 -0500

    added sites to leak email on

--- demo.py
+++ demo.py
@@ -10,7 +10,8 @@ from openwpm.task_manager import TaskManager
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 1
 sites = [
-    "http://www.google.com",
+    "https://razor.com/",
+    "https://www.rvtrader.com/"
 ]
 
 # Loads the default ManagerParams
